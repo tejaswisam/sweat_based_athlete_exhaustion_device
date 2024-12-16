1199708768016c8579311a3bb3a72fa863706b9c
Tejaswi Samavedula <tejaswisamavedula@gmail.com>
2024-12-16 16:10:41 +0530

    first commit

---
+++ esp82_sih_project.ino
@@ -0,0 +1,58 @@
+#define BLYNK_TEMPLATE_ID "TMPL3WnrYzhiM"
+#define BLYNK_TEMPLATE_NAME "fatigueanalysis"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+
+char auth[] = "knWC_8TSEoG6RRJWBX905QPQsOvWWYZq";
+char ssid[] = "TEJASWI";
+char pass[] = "12345678"; 
+
+DHT dht(D3, DHT11);  // D4 DHT11
+BlynkTimer timer;
+
+#define sweat A0
+
+void setup() {
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 8080);
+  dht.begin();
+  
+  timer.setInterval(100L, sweatSensor);
+  timer.setInterval(100L, DHT11sensor);
+}
+
+void DHT11sensor() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Blynk.virtualWrite(V0, t);
+  Blynk.virtualWrite(V1, h);
+}
+
+void sweatSensor() {
+  int value = analogRead(sweat);
+  value = map(value, 0, 1024, 0, 100);
+  value = (value - 100) * -1;
+  
+  Blynk.virtualWrite(V3, value);
+  if (value < 30) {
+    Blynk.virtualWrite(V4, HIGH);
+  } else if (value > 60) {
+    Blynk.virtualWrite(V4, LOW);
+  }
+
+}
+
+void loop() {
+  Blynk.run(); 
+  timer.run();
+}
